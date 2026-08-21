d6a3baf814b4597e8d675815f9e7ae82192fea0e
dimasabdulrohman-xi-rpl2 <dimasabdulrohman84@gmail.com>
2026-08-21 16:03:58 +0700

    membuat migrasi user dan profile

---
+++ database/migrations/2026_08_21_070356_create_user_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table)
+        {
+            $table->id();
+            $table->string('name', 45);
+            $table->string('email', 45);
+            $table->string('password', 45);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
